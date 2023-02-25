02a32884ffffcfc77be396a2eed1dcbd72d96e8a
Chandrashekhar Mistry <113460278+ChandrashekharMistry@users.noreply.github.com>
2023-02-25 13:27:07 +0530

    Adding files to git

---
+++ main.js
@@ -0,0 +1,12 @@
+const multiply = require("./calc/multiply.js.js")
+
+addition= require("./calc/sum.js.js")
+minus= require("./calc/subtract.js.js")
+divide= require("./calc/divide.js.js")
+multip= require("./calc/multiply.js.js")
+
+
+result= addition(1,2)
+result= minus(1,2)
+result= divide(1,2)
+result= multip(1,2)
